15428f670572b114b6720a706a1822669bf6761b
Pat <162609559+Devopspat35@users.noreply.github.com>
2024-03-07 12:06:08 -0500

    Update atemnkeng.sh
    names added

--- atemnkeng.sh
+++ atemnkeng.sh
@@ -1,2 +1,19 @@
 God is ever great to all the earth
+foncha njiaka
+abunga
+atemnkeng
+tazifor
+nkeng
+ajua
+asong
+shorita
+gilbert
+atabong
+ache 
+sylvia
+roland
+nkemabanga
+tateh
+ngenyi
+That peace main reign now and forevermore. 
 And so shall it be now and forevermoer ... Amen
